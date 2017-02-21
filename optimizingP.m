b635f2394aaa0129a55b20989f373f60c60f98ab
Zhengming Ding <allanding@ece.neu.edu>
2017-02-21 14:10:09 -0500

    Update optimizingP.m

--- optimizingP.m
+++ optimizingP.m
@@ -1,10 +1,12 @@
 function P = optimizingP(P,Y,Lw,X,Q,Z,n,la0,E,Y1,Y3,mu,maxIter)
-opts.record = 0;
-opts.mxitr  = maxIter;
-opts.xtol = 1e-3;
-opts.gtol = 1e-3;
-opts.ftol = 1e-4;
-opts.tau = 1e-3;
+  opts.record = 0;
+  opts.mxitr  = maxIter;
+  opts.xtol = 1e-3;
+  opts.gtol = 1e-3;
+  opts.ftol = 1e-4;
+  opts.tau = 1e-3;
 
-P=OptStiefelGBB(P, @objHereInner, opts, X,Y,Lw,Z,E,Q,Y1,Y3,n,la0,mu);
+  %% optimize P by call objHereInnner
+
+  P=OptStiefelGBB(P, @objHereInner, opts, X,Y,Lw,Z,E,Q,Y1,Y3,n,la0,mu);
 end
